3ed64a8723f14ef3c6adc1ccb756a3aa97978d60
Hamza Chakoubi <hamza.chakoubi28@gmail.com>
2022-09-22 13:41:42 +0100

    ex01 updated

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hchakoub <hchakoub@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/21 20:24:32 by hchakoub          #+#    #+#             */
+/*   Updated: 2022/09/22 08:17:57 by hchakoub         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+#include <iostream>
+#include <stack>
+
+template <typename T> class MutantStack : public std::stack<T> {
+
+public:
+  typedef typename std::stack<T>::container_type::iterator iterator;
+  typedef typename std::stack<T>::container_type::reverse_iterator riterator;
+
+  iterator begin() { return this->c.begin(); }
+  riterator rbegin() { return this->c.rbegin(); }
+  iterator end() { return this->c.end(); }
+  riterator rend() { return this->c.rend(); }
+};
+#endif // !MUTANTSTACK_HPP
